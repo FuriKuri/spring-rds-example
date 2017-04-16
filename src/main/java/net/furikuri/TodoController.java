e71ed8059b2f3254015ff175461424b7de728e9e
Theo Pack Test <tf.pack@gmail.com>
2017-04-16 09:10:32 +0200

    Add endpoint for saving todo

--- src/main/java/net/furikuri/TodoController.java
+++ src/main/java/net/furikuri/TodoController.java
@@ -1,7 +1,9 @@
 package net.furikuri;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +17,10 @@ public class TodoController {
     @RequestMapping("/")
     public List<Todo> getAll() {
         return repository.findAll();
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Todo save(@RequestBody Todo todo) {
+        return repository.save(todo);
     }
 }
